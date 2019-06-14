2a3459484bf6adbdbb8e365b2073e1483b8b9e26
songwei <songwei@zhizhangyi.com>
2019-06-14 14:17:58 +0800

    commit Q_7

---
+++ src/Coding_Interviews_Java/_07_ConstructBinaryTree/ConstructBinaryTree.java
@@ -0,0 +1,31 @@
+package Coding_Interviews_Java._07_ConstructBinaryTree;
+
+/**
+ *
+ * 面试题7：重建二叉树
+ *
+ * 题目：输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * 例如，输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，
+ * 则重建如图2.6所示的二叉树并输出它的头结点。
+ * 二叉树节点的定义如下：
+ * struct BinaryTreeNode{
+ *     int m_nValue;
+ *     BinaryTreeNode* m_pLeft;
+ *     BinaryTreeNode* m_pRight;
+ * };
+ *
+ *
+ */
+public class ConstructBinaryTree {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+    public static void constructBinaryTree(){
+        //TODO: fixme
+    }
+
+
+}
