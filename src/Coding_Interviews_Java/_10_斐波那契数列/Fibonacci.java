c350dd0c6749dfb02248022e04ac5356d845309b
songwei <925360166@qq.com>
2019-06-17 01:12:30 +0800

    commit Q_10

---
+++ src/Coding_Interviews_Java/_10_斐波那契数列/Fibonacci.java
@@ -0,0 +1,54 @@
+package Coding_Interviews_Java._10_斐波那契数列;
+
+/**
+ *
+ * 面试题10：斐波那契数列
+ *
+ * 题目一：求斐波那契数列的第n项。
+ * 写一个函数，输入n，求斐波那契（Fibonacci）数列的第n项。
+ * 斐波那契数列的定义如下：
+ *         0                n=0
+ * f(n) =  1                n=1
+ *         f(n-1) + f(n-2)  n>1
+ *
+ */
+public class Fibonacci {
+
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+
+    }
+
+    /**
+     * 1、用递归方法计算的时间复杂度是以n的指数的方式递增的。
+     * 递归调用过程的树形结构中的重复节点会随着n的增大而急剧增大《剑指Offer（第二版）》P75
+     */
+
+    public static long Fibonacci1(int n){
+        if(n <= 1)
+            return n;
+        else
+            return Fibonacci1(n-1) + Fibonacci1(n-2);
+    }
+
+
+    /**
+     * 2、面试官期待的实用解法
+     * 更简单的方式是从下往上计算，首先根据f(0)和f(1)算出f(2),
+     * 再根据f(1)和f(2)算出f(3)......以此类推就可以算出第n项了。
+     * 很容量理解，这种思路的时间复杂度是O(n)
+     *
+     */
+    public static long Fibonacci2(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fn1 = 0;
+        int fn2 = 1;
+        for(int i = 2; i <= n; i++){
+            fn2 = fn1 + fn2;
+            fn1 = fn2 - fn1;
+        }
+        return fn2;
+    }
+}
---
+++ src/Coding_Interviews_Java/_10_斐波那契数列/Fibonacci_FrogJumpMethods.java
@@ -0,0 +1,71 @@
+package Coding_Interviews_Java._10_斐波那契数列;
+
+/**
+ * 题目2：青蛙跳台阶问题
+ *
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶
+ * 求该青蛙跳上一级n级的台阶总共有多少种跳法
+ *
+ * 解答：https://blog.csdn.net/weixin_40244153/article/details/87730450
+ */
+public class Fibonacci_FrogJumpMethods {
+
+    public static void main(String[] args){
+        System.out.print("6级台阶的方法数：" + solution2(6));
+    }
+
+    /**
+     *
+     * 答题思路
+     *
+     * 1.如果只有1级台阶，那显然只有一种跳法
+     * 2.如果有2级台阶，那么就有2种跳法，一种是分2次跳。每次跳1级，另一种就是一次跳2级
+     * 3.如果台阶级数大于2，设为n的话，这时我们把n级台阶时的跳法看成n的函数，记为f(n),
+     *   第一次跳的时候有2种不同的选择：
+     *   一是第一次跳一级，此时跳法的数目等于后面剩下的n-1级台阶的跳法数目，即为f(n-1),
+     *   二是第一次跳二级，此时跳法的数目等于后面剩下的n-2级台阶的跳法数目，即为f(n-2),
+     *   因此n级台阶的不同跳法的总数为，不难看出就是斐波那契数列
+     *
+     */
+
+    /**
+     * 递归方法
+     *
+     * 时间复杂度O(2^n),空间复杂度O(n)。
+     */
+    public static int solution1(int n){
+        if(n <= 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        return solution1(n - 1) + solution1(n - 2);
+    }
+
+    /**
+     * 循环实现
+     *
+     * @param n
+     * @return
+     */
+    public static int solution2(int n){
+        int[] res = {0, 1};
+        if(n < 2){
+            return res[n];
+        }
+
+        int fibMinusOne = 1;
+        int fibMinusTwo = 0;
+        int fibN = 0;
+        for(int i = 2; i <= n; i++){
+            fibN = fibMinusOne + fibMinusTwo;
+            fibMinusTwo = fibMinusOne;
+            fibMinusOne = fibN;
+        }
+        return fibN;
+    }
+}
