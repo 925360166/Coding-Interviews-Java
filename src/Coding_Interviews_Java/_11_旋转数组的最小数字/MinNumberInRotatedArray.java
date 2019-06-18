4be8735c77350e240437c0deff3da0e10dcc75f2
songwei <925360166@qq.com>
2019-06-19 01:28:40 +0800

    commit Q_11

--- src/Coding_Interviews_Java/_11_旋转数组的最小数字/MinNumberInRotatedArray.java
+++ src/Coding_Interviews_Java/_11_旋转数组的最小数字/MinNumberInRotatedArray.java
@@ -11,4 +11,87 @@ package Coding_Interviews_Java._11_旋转数组的最小数字;
  *
  */
 public class MinNumberInRotatedArray {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        int[] numbers = {3,4,5,1,2};
+        int min = solution(numbers, numbers.length);
+        System.out.println("最小值：" + min);
+
+        int[] numbers2 = {1,1,1,0,1};
+        int min2 = solutionBetter(numbers2, numbers2.length);
+        System.out.println("最小值：" + min2);
+    }
+
+    //第一种解法
+    private static int solution(int[] numbers, int length) {
+        if(numbers == null || length <= 0){
+            throw new RuntimeException("Invalid parameters");
+        }
+
+        int first = 0;
+        int last = length - 1;
+        int middle = first;
+        while(numbers[first] >= numbers[last]){
+            if(last - first == 1){
+                middle = last;
+                break;
+            }
+            middle = (first + last)/2;
+            if(numbers[middle] >= numbers[first]){
+                first = middle;
+            }else if(numbers[middle] <= numbers[last]){
+                last = middle;
+            }
+        }
+        return numbers[middle];
+    }
+
+    //解法的改进
+
+    /**
+     * 考虑例子{1,0,1,1,1}、{1,1,1,0,1}
+     *
+     *
+     * @param numbers
+     * @param length
+     * @return
+     */
+    private static int solutionBetter(int[] numbers, int length){
+        if(numbers == null || length <= 0){
+            throw new RuntimeException("Invalid parameters");
+        }
+        int first = 0;
+        int last = length - 1;
+        int middle = first;
+        while(numbers[first] >= numbers[last]){
+            if(last - first == 1){
+                middle = last;
+                break;
+            }
+            middle = (first + last) / 2;
+            //如果下标为firs、last和middle指向的三个数字相等，
+            //则只能顺序查找
+            if(numbers[first] == numbers[last] && numbers[first] == numbers[middle]){
+                return minInOrder(numbers, first, last);
+            }
+            if(numbers[middle] >= numbers[first]){
+                first = middle;
+            }else if(numbers[middle] <= numbers[last]){
+                last = middle;
+            }
+        }
+        return numbers[middle];
+    }
+
+    private static int minInOrder(int[] numbers, int first, int last){
+        int result = numbers[first];
+        for(int i = first + 1; i <= last; i++){
+            if(numbers[i] < result){
+                result = numbers[i];
+            }
+        }
+        return result;
+    }
 }
