83ddf139a9b19e16990c6d70f2c8549b91cb64c4
songwei <925360166@qq.com>
2019-06-23 23:15:27 +0800

    add a very hard question

---
+++ src/Very_Hard/Recursive_BackTracking_Watch_Soilders.java
@@ -0,0 +1,14 @@
+package Very_Hard;
+
+/**
+ *
+ * https://www.cnblogs.com/brifuture/p/6553665.html
+ *
+ * 题目描述
+ * 有n个士兵站成一列，从第1个士兵前面向后望去，刚好能看到m个士兵，
+ * 如果站在后面的士兵身高小于或者等于前面某个士兵的身高，那么后面的这个士兵就不能被看到，
+ * 问这n个士兵有多少种排列方式，刚好在观测位能看到m个士兵？
+ *
+ */
+public class Recursive_BackTracking_Watch_Soilders {
+}
