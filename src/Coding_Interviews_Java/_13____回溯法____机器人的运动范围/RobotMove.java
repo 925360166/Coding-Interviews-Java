2dcbc2291b84887dbba879412387290b20164f77
songwei <songwei@zhizhangyi.com>
2019-06-25 15:11:36 +0800

    fix Q_13

--- src/Coding_Interviews_Java/_13____回溯法____机器人的运动范围/RobotMove.java
+++ src/Coding_Interviews_Java/_13____回溯法____机器人的运动范围/RobotMove.java
@@ -11,13 +11,66 @@ package Coding_Interviews_Java._13____回溯法____机器人的运动范围;
  */
 public class RobotMove {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(movingCount(18, 18, 18));
     }
 
 
-    private static int movingCount() {
-        //TODO fixme!
+    /**
+     *
+     * @param threshold 门槛，阀，界
+     * @param rows 矩阵的行
+     * @param cols 矩阵的列
+     * @return 机器人能够到达的格子的数量
+     */
+    private static int movingCount(int threshold, int rows, int cols){
+        if(threshold < 0 || rows <= 0 || cols <= 0){
+            return 0;
+        }
+        boolean[] visited = new boolean[rows * cols];
+        for(int i = 0; i < rows * cols; i++){
+            visited[i] = false;
+        }
+        int count = movingCountCore(threshold, rows, cols, 0, 0, visited);
+//        delete[] visited;
+        return count;
 
-        return 0;
     }
+
+    private static int movingCountCore(int threshold, int rows, int cols, int row, int col, boolean[] visited) {
+        int count = 0;
+        if(check(threshold, rows, cols, row, col, visited)){
+            visited[row * cols + col] = true;
+            count = 1 + movingCountCore(threshold, rows, cols, row - 1, col, visited)
+                      + movingCountCore(threshold, rows, cols, row, col - 1, visited)
+                      + movingCountCore(threshold, rows, cols, row + 1, col, visited)
+                      + movingCountCore(threshold, rows, cols, row, col + 1, visited);
+        }
+        return count;
+    }
+
+    // 判断机器人能否进入坐标为（row, col）的方格，
+    // 而函数getDigitSum用来得到一个数字的数位之和
+    private static boolean check(int threshold, int rows, int cols, int row, int col, boolean[] visited){
+        if(row >= 0 && row < rows
+                && col >= 0 && col < cols
+                && getDigitSum(row)+getDigitSum(col)<= threshold
+                && !visited[row * cols + col]){
+            return true;
+        }
+        return false;
+    }
+
+    //用来的到一个数字的各数位之和。如getDigitSum(35) == 8
+    private static int getDigitSum(int number){
+        int sum = 0;
+        while(number > 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+
+
+
 }
