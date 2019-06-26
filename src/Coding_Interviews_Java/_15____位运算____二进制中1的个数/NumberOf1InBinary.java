bc3095c504cca0f34acb694fc5e6725aa3918b6b
songwei <songwei@zhizhangyi.com>
2019-06-26 13:57:46 +0800

    add Q_15

---
+++ src/Coding_Interviews_Java/_15____位运算____二进制中1的个数/NumberOf1InBinary.java
@@ -0,0 +1,18 @@
+package Coding_Interviews_Java._15____位运算____二进制中1的个数;
+
+/**
+ * 面试题15：二进制中1的个数
+ *
+ * 题目：请实现一个函数，输入一个整数，输出该数二进制表示中1的个数。
+ * 例如：把9表示成二进制是1001，有2位是1。
+ * 因此，如果输入9，则该函数输出2。
+ *
+ */
+public class NumberOf1InBinary {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+
+}
