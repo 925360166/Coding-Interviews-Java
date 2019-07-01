54ef0e23c383d9bb1af5e24cb6ffb66aebb64ed8
songwei <925360166@qq.com>
2019-07-01 22:21:34 +0800

    commit Q_17

---
+++ src/Coding_Interviews_Java/_17____打印从1到最大的n位数/Print1ToMaxOfNDigits.java
@@ -0,0 +1,21 @@
+package Coding_Interviews_Java._17____打印从1到最大的n位数;
+
+/**
+ * 面试题17：打印从1到最大的n位数
+ *
+ * 题目：
+ * 输入数字n，按顺序打印出从1到最大的n位的十进制数。
+ * 比如输入3，则打印出1、2、3一直到最大的3位数999.
+ *
+ */
+public class Print1ToMaxOfNDigits {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+    private static void solution(){
+
+    }
+
+}
